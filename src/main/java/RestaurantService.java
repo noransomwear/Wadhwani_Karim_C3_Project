4d1870ce95749fe324eeaa1f3466f4450cba01e0
Karim Wadhwani <inboxkarim@hotmail.com>
2022-06-08 19:42:19 +0530

    Part 2: Solution

--- src/main/java/RestaurantService.java
+++ src/main/java/RestaurantService.java
@@ -2,12 +2,25 @@ import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
 public class RestaurantService {
     private static List<Restaurant> restaurants = new ArrayList<>();
 
-    public Restaurant findRestaurantByName(String restaurantName){
-        return null;
-        //DELETE ABOVE STATEMENT AND WRITE CODE HERE
+    public Restaurant findRestaurantByName(String restaurantName) throws restaurantNotFoundException {
+        for (Restaurant restaurant : restaurants) {
+            if(restaurant.getName().equalsIgnoreCase(restaurantName)){
+                return restaurant;
+            }
+    }
+        throw new restaurantNotFoundException(restaurantName);
+
     }
 
 
